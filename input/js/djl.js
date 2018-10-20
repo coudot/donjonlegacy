915bac945a050f3f94fd884b361fa727dcb1c18f
Clément OUDOT <clement@oodo.net>
2018-10-20 11:38:19 +0200

    Javascript pour affichage des événements

---
+++ input/js/djl.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$.getJSON( "/events.json", function( data ) {
+		var items = [];
+		$.each( data, function( key, val ) {
+			var now = Date.now();
+			var start = new Date(val.start);
+			var end = new Date(val.end);
+			var time = end.getTime();
+			var display = start.getDate() + "/" + (start.getMonth() + 1) + "/" + start.getFullYear();
+			if ( end.getDate() > start.getDate() ) {
+				display = display + " - " + end.getDate() + "/" + (end.getMonth() + 1) + "/" + end.getFullYear();
+			}
+			if (time > now ) {
+				items.push( "<p id='" + key + "'>" + display + "<br /><a href="+val.link+" target='_blank'>" + val.name + "</a></p>" );
+			}
+		});
+
+		$( "<div/>", {
+			"class": "agenda",
+			html: items.join( "" )
+		}).appendTo( "div.events" );
+	});
+});
